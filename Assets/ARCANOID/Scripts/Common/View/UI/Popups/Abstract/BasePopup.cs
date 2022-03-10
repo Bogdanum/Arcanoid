07f4a9bff5caf1d7a834e04444c2f1680816ba37
Bogdanum <bhololeenko@mail.ru>
2022-03-10 13:55:25 +0300

    popup localization fix

--- Assets/ARCANOID/Scripts/Common/View/UI/Popups/Abstract/BasePopup.cs
+++ Assets/ARCANOID/Scripts/Common/View/UI/Popups/Abstract/BasePopup.cs
@@ -2,17 +2,25 @@ using System;
 using System.Collections;
 using UnityEngine;
 
-public abstract class BasePopup : PoolableUIEntity
+public abstract class BasePopup : MonoBehaviour
 {
     [SerializeField] private PopupAnimationController animationController;
-
-    public override void Init()
+    
+    public void Init()
     {
+        SetupScaleAndPosition();
         gameObject.SetActive(false);
+        
         if (animationController != null)
         {
             animationController.Init();
         }
+    }
+
+    private void SetupScaleAndPosition()
+    {
+        var rect = (RectTransform) transform;
+        rect.RefreshScaleAndPosition();
     }
 
     public IEnumerator Show(Action onComplete)
